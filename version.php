98fcafe8db58e5c0e80724208ea1a1a392e39fd0
Olumuyiwa Taiwo <muyi.taiwo@logicexpertise.com>
2024-05-29 23:02:00 +1200

    Remove settings.php

--- version.php
+++ version.php
@@ -10,6 +10,6 @@
  */
 
 $plugin->component = 'mod_activitystatus';
-$plugin->version = 2024052900;
+$plugin->version = 2024052901;
 $plugin->requires = 2015051107;
 $plugin->maturity = MATURITY_RC;
